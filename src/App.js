5f21b31d9b63a4c837ca0358c5054b3ebf764845
martsDev <martssilvapro@gmail.com>
2022-10-17 18:30:32 -0300

    add project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import './App.css';
 import Text from './Text';
 
-import { motion, useScroll, useSpring } from 'framer-motion';
+import { motion, useScroll } from 'framer-motion';
 
 function App() {
 
   const { scrollYProgress } = useScroll();
 
-  const scaleX = useSpring(scrollYProgress, {
-    stiffness: 100,
-    damping: 30,
-    restDelta: 0.001
-  })
-
   return (
     <section className="container">
       <motion.div 
         className='progress-bar'
-        style={{ scaleX }}
+        style={{ scaleX: scrollYProgress }}
       />
       <Text />
     </section>
